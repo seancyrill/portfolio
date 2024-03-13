15c0a6dd824cdc58712eb709cee8d19fbca9b60f
Sean <seancyrill@gmail.com>
2024-03-14 06:30:19 +0800

    scroll performance fix 6

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -4,7 +4,7 @@ import { ReactSVG } from 'react-svg'
 function ScrollToTop() {
     const [isVisible, setIsVisible] = useState(false)
 
-    useEffect(() => {
+    /* useEffect(() => {
         const toggleVisibility = () => {
             if (window.scrollY > window.innerHeight) {
                 !isVisible && setIsVisible(true)
@@ -16,7 +16,7 @@ function ScrollToTop() {
         window.addEventListener('scroll', toggleVisibility)
 
         return () => window.removeEventListener('scroll', toggleVisibility)
-    }, [])
+    }, []) */
 
     function scrollToTop() {
         window.scrollTo({

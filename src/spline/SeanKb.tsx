95764beb81a90ffb5777dfb3b52102a5fef50966
Sean <seancyrill@gmail.com>
2024-03-14 22:59:48 +0800

    removed 1 3dkb

--- src/spline/SeanKb.tsx
+++ src/spline/SeanKb.tsx
@@ -8,7 +8,7 @@ function SeanKb({ setSplinesLoaded }: SeanKbType) {
     return (
         <Spline
             scene="https://prod.spline.design/f6yoVX8kfTXlrFTL/scene.splinecode"
-            className="absolute"
+            className="absolute hidden"
             onLoad={() =>
                 setSplinesLoaded((prev) =>
                     prev.map((state, i) => (i === 1 ? true : state))

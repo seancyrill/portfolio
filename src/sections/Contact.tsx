2b78bb308a06405cba76e76900c105e80191de15
Sean <seancyrill@gmail.com>
2024-03-15 19:58:10 +0800

    added netlify form submission 10

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -23,6 +23,8 @@ function Contact({ setSplinesLoaded }: ContactType) {
         >
             <div className="relative flex w-full rounded-3xl bg-secondary-neutral text-primary-neutral portrait:flex-col landscape:flex-row">
                 <div className="relative flex flex-col justify-center gap-4 border-primary-neutral px-8 py-2 sm:py-8 portrait:h-1/2 portrait:w-full portrait:border-b landscape:h-full landscape:w-1/2 landscape:border-r">
+                    {/* for netlify form detection */}
+
                     <h1 className="w-full p-2 font-header text-6xl font-bold sm:p-8 sm:text-8xl xl:text-[8rem] portrait:text-center landscape:text-right">
                         GET IN TOUCH
                     </h1>
@@ -31,16 +33,8 @@ function Contact({ setSplinesLoaded }: ContactType) {
                         className="flex flex-col place-content-center gap-2 sm:gap-4"
                         name="contact"
                         method="post"
-                        data-netlify="true"
-                        data-netlify-honeypot="bot-field"
-                        data-netlify-recaptcha="true"
                     >
-                        {/* for netlify form detection and spam detection */}
                         <input type="hidden" name="form-name" value="contact" />
-                        <div hidden>
-                            <input name="bot-field" />
-                        </div>
-                        {/* for netlify form detection and spam detection */}
 
                         <label className="sr-only">Name</label>
                         <input
@@ -68,7 +62,7 @@ function Contact({ setSplinesLoaded }: ContactType) {
                         />
 
                         <div data-netlify-recaptcha="true"></div>
-                        <button type="submit">Send</button>
+                        <button>Send</button>
                     </form>
                 </div>
 

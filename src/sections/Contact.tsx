4613b150b9280d9b6a95c73a29f266bf49709c1f
Sean <seancyrill@gmail.com>
2024-03-15 16:49:44 +0800

    added netlify form submission 3

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -26,35 +26,35 @@ function Contact({ setSplinesLoaded }: ContactType) {
                     className="relative flex flex-col justify-center gap-4 border-primary-neutral px-8 py-2 sm:py-8 portrait:h-1/2 portrait:w-full portrait:border-b landscape:h-full landscape:w-1/2 landscape:border-r"
                     name="contact"
                     method="POST"
-                    data-netlify="true"
                 >
-                    {
-                        // for netlify form detection
-                        <input type="hidden" name="form-name" value="contact" />
-                    }
+                    {/* for netlify form detection */}
+                    <input type="hidden" name="form-name" value="contact" />
 
                     <h1 className="w-full p-2 font-header text-6xl font-bold sm:p-8 sm:text-8xl xl:text-[8rem] portrait:text-center landscape:text-right">
                         GET IN TOUCH
                     </h1>
                     <div className="flex flex-col place-content-center gap-2 sm:gap-4">
+                        <label className="sr-only">Name</label>
                         <input
                             type="text"
                             name="name"
                             placeholder="Company | Name"
                             className="border-x border-primary-neutral bg-transparent px-4 py-2 focus:outline-none"
                         />
+
+                        <label className="sr-only">Email</label>
                         <input
                             name="email"
                             type="email"
                             placeholder="Email"
                             className="border-x border-primary-neutral bg-transparent px-4 py-2 focus:outline-none"
                         />
+                        <label className="sr-only">Message</label>
                         <textarea
                             name="message"
                             placeholder="Message"
                             className="z-10 border-x border-primary-neutral bg-transparent px-4 py-2 focus:outline-none"
                         />
-                        <div data-netlify-recaptcha="true" />
                         <button>Send</button>
                     </div>
                 </form>
